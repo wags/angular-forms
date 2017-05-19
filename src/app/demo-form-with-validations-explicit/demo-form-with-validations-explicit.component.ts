8971e80883c7a45c7901e6eb79b8189e8f62e69a
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-19 10:14:21 -0400

    Remove unused OnInit

--- src/app/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
+++ src/app/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -10,7 +10,7 @@ import {
   selector: 'app-demo-form-with-validations-explicit',
   templateUrl: './demo-form-with-validations-explicit.component.html'
 })
-export class DemoFormWithValidationsExplicitComponent implements OnInit {
+export class DemoFormWithValidationsExplicitComponent {
   myForm: FormGroup;
   sku: AbstractControl;
 
@@ -20,9 +20,6 @@ export class DemoFormWithValidationsExplicitComponent implements OnInit {
     });
 
     this.sku = this.myForm.controls['sku'];
-  }
-
-  ngOnInit() {
   }
 
   onSubmit(value: string): void {
